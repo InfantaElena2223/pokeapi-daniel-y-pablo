803557b0ebeff15fb370680482daeaffeac2047c
ppolo1 <pppcuenca@gmail.com>
2023-04-24 20:54:20 +0200

    "hola"

--- src/org/infantaelena/Main.java
+++ src/org/infantaelena/Main.java
@@ -2,6 +2,12 @@ package org.infantaelena;
 
 import org.infantaelena.controlador.Controlador;
 
+/**
+ * @author Daniel Ocaña y Pablo Polo
+ * @version 1.0
+ * @date 24/04/2023
+ */
+
 public class Main
 
 {
