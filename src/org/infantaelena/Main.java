a313cac7fdb9034635b4deff0f520459dd9d4ad6
ppolo1 <pppcuenca@gmail.com>
2023-04-24 13:19:50 +0200

    "hola"

--- src/org/infantaelena/Main.java
+++ src/org/infantaelena/Main.java
@@ -7,10 +7,7 @@ public class Main
 {
 
     public static void main(String args[]) {
-        System.out.println("hola");
-        System.out.println("perezaaa");
         new Controlador();
-
     }
 
 }
