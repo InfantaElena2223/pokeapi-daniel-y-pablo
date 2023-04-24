75a1e71aff2cf09eb0cb54a8c99e88a99061541b
ppolo1 <pppcuenca@gmail.com>
2023-04-24 13:02:05 +0200

    "hola"

--- src/org/infantaelena/Main.java
+++ src/org/infantaelena/Main.java
@@ -8,6 +8,7 @@ public class Main
 
     public static void main(String args[]) {
         System.out.println("hola");
+        System.out.println("Somos tontos");
         new Controlador();
 
     }
