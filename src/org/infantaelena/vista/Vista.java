7ad5f6207ba6e446a9cbfd8906938b36597a96b4
daniotimon28 <daniotimon@gmail.com>
2023-04-25 10:16:56 +0200

    "prueba"

--- src/org/infantaelena/vista/Vista.java
+++ src/org/infantaelena/vista/Vista.java
@@ -35,6 +35,76 @@ public class Vista {
         return sc.nextInt();
     }
 
-public void
+    public String pedirNombre() {
+        System.out.print("Introduce el nombre: ");
+        String nombre = sc.nextLine();
+        return nombre;
+    }
 
+    public Pokemon.Clases pedirClase() {
+
+        Pokemon.Clases clase = Pokemon.Clases.SIN_CLASE;
+        System.out.println("Introduce la clase");
+        System.out.print("Opciones disponibles [SIN_CLASE,FUEGO, TIERRA, AIRE, AGUA]: ");
+
+        String textoClase = sc.nextLine().toUpperCase();
+        try {
+            clase = Pokemon.Clases.valueOf(textoClase);
+        } catch (IllegalArgumentException e) {
+            System.err.println("No se ha encontrado la clase prueba otra vez");
+            pedirClase();
+        }
+        return clase;
+
+    }
+
+    public int pedirVida() {
+        System.out.print("Introduce la vida");
+        int vida = sc.nextInt();
+        do {
+            if (vida <= 0 || vida > 50) {
+                System.err.println("La vida no es válida");
+                System.out.print("Introducela de nuevo");
+            }
+        } while (vida <= 0 || vida > 50);
+        return vida;
+    }
+
+    public int pedirDefensa() {
+        System.out.print("Introduce la Defensa");
+        int defensa = sc.nextInt();
+        do {
+            if (defensa < 0 || defensa > 50) {
+                System.err.println("La defensa no es válida");
+                System.out.print("Introducela de nuevo");
+            }
+        } while (defensa < 0 || defensa > 50);
+        return defensa;
+    }
+
+    public int pedirAtaque() {
+        System.out.print("Introduce el Ataque");
+        int ataque = sc.nextInt();
+        do {
+            if (ataque < 0 || ataque > 50) {
+                System.err.println("La ataque no es válida");
+                System.out.print("Introducela de nuevo");
+            }
+        } while (ataque < 0 || ataque > 50);
+        return ataque;
+    }
+
+    public int pedirVelocidad() {
+        System.out.print("Introduce la velocidad");
+        int velocidad = sc.nextInt();
+        do {
+            if (velocidad < 0 || velocidad > 50) {
+                System.err.println("La velocidad no es válida");
+                System.out.print("Introducela de nuevo");
+            }
+        } while (velocidad < 0 || velocidad > 50);
+        System.out.println();
+        return velocidad;
+
+    }
 }
