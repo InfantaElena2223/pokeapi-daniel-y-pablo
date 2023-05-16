17640751632d82a4caa27ecdb1aede18eafff026
daniotimon28 <daniotimon@gmail.com>
2023-05-16 10:16:05 +0200

    "16-5"

--- src/org/infantaelena/modelo/dao/PokemonDAOImp.java
+++ src/org/infantaelena/modelo/dao/PokemonDAOImp.java
@@ -1,12 +1,12 @@
 package org.infantaelena.modelo.dao;
 
-import org.h2.jdbc.JdbcSQLIntegrityConstraintViolationException;
 import org.infantaelena.excepciones.PokemonNotFoundException;
 import org.infantaelena.excepciones.PokemonRepeatedException;
 import org.infantaelena.modelo.entidades.Pokemon;
 
 import java.sql.*;
 import java.util.List;
+
 
 /**
  * Clase que implementa los métodos de acceso a datos de la clase Pokemon
@@ -21,80 +21,81 @@ public class PokemonDAOImp implements PokemonDAO {
     private Connection connection;
 
     public PokemonDAOImp() {
-        Statement statement = null;
-        Connection connection = null;
+
         try {
             connection = DriverManager.getConnection("jdbc:h2:./data/pokeapi", "sa", "");
 
-
         } catch (SQLException e) {
             System.err.println("Error al hacer operaciones con la base de datos");
         }
 
+        crearTablaPokeapi();
 
-        crearTablaPokeapi(connection);
-        try {
 //Insertar varias filas
-            String insert1 = "INSERT INTO pokeapi (nombre,clase,vida,defensa,ataque,velocidad) VALUES ('Pikachu','FUEGO',0,0,0,0);";
-            statement.executeUpdate(insert1);
+           /* try{
+                String insert1 = "INSERT INTO pokeapi (nombre,clase,vida,defensa,ataque,velocidad) VALUES ('Pikachu','FUEGO',0,0,0,0);";
+                statement.executeUpdate(insert1);
+            }catch (JdbcSQLIntegrityConstraintViolationException e) {
+                System.err.println("El pokémon ya existe");
+            }*/
 
-
-            //Actualizar una fila
+        //Actualizar una fila
       /*      String update = "UPDATE pokeapi SET email = 'juan@yahoo.com' WHERE nombre = 'Juan';";
             statement.executeUpdate(update);*/
 
-            //Borrar una fila
+        //Borrar una fila
             /*String delete = "DELETE FROM pokeapi WHERE nombre = 'María';";
             statement.executeUpdate(delete);*/
 
-            //Imprimir todo el contenido de la tabla
-            String selectAll = "SELECT * FROM pokeapi;";
+        //Imprimir todo el contenido de la tabla
+        try (Statement statement= connection.createStatement()){
+        String selectAll = "SELECT * FROM pokeapi;";
 
-            ResultSet resultSet = null;
-            try {
-                resultSet = statement.executeQuery(selectAll);
-            } catch (SQLException e) {
-            }
-            while (resultSet.next()) {
-                String nombre = resultSet.getString("nombre");
-                String clase = resultSet.getString("clase");
-                int vida = resultSet.getInt("vida");
-                int defensa = resultSet.getInt("defensa");
-                int ataque = resultSet.getInt("ataque");
-                int velocidad = resultSet.getInt("velocidad");
-                System.out.println(nombre + "\t" + clase + "\t" + vida + "\t" + defensa + "\t" + ataque + "\t" + velocidad);
-            }
-            while (resultSet.next()) {
-                String nombre = resultSet.getString("nombre");
-                String clase = resultSet.getString("clase");
-                int vida = resultSet.getInt("vida");
-                int defensa = resultSet.getInt("defensa");
-                int ataque = resultSet.getInt("ataque");
-                int velocidad = resultSet.getInt("velocidad");
-                System.out.println(nombre + "\t" + clase + "\t" + vida + "\t" + defensa + "\t" + ataque + "\t" + velocidad);
-            }
+        ResultSet resultSet = null;
 
-            //Cerrar la conexión a la base de datos
-            statement.close();
-            connection.close();
-        } catch (SQLException e) {
-            System.err.println("Error al hacer operaciones con la base de datos");
-        } catch (Exception e) {
-            e.printStackTrace();
+            resultSet = statement.executeQuery(selectAll);
+
+        while (resultSet.next()) {
+            String nombre = resultSet.getString("nombre");
+            String clase = resultSet.getString("clase");
+            int vida = resultSet.getInt("vida");
+            int defensa = resultSet.getInt("defensa");
+            int ataque = resultSet.getInt("ataque");
+            int velocidad = resultSet.getInt("velocidad");
+            System.out.println(nombre + "\t" + clase + "\t" + vida + "\t" + defensa + "\t" + ataque + "\t" + velocidad);
         }
-    }
+        while (resultSet.next()) {
+            String nombre = resultSet.getString("nombre");
+            String clase = resultSet.getString("clase");
+            int vida = resultSet.getInt("vida");
+            int defensa = resultSet.getInt("defensa");
+            int ataque = resultSet.getInt("ataque");
+            int velocidad = resultSet.getInt("velocidad");
+            System.out.println(nombre + "\t" + clase + "\t" + vida + "\t" + defensa + "\t" + ataque + "\t" + velocidad);
+        }
+        } catch (SQLException e) {
+            System.err.println("Error al realizar la consulta");
+        }
+        //Cerrar la conexión a la base de datos
+     /*   statement.close();
+        connection.close();*/
+    /*} catch(SQLException e){
+        System.err.println("Error al hacer operaciones con la base de datos");
+    } catch(Exception e){
+        e.printStackTrace();
+    }*/
+}
 
-    private static void crearTablaPokeapi(Connection connection) {
+    private void crearTablaPokeapi() {
 
         try (Statement statement = connection.createStatement()) {
+
             //Crear tabla
             String createTableSQL = "CREATE TABLE IF NOT EXISTS pokeapi (\n" + " nombre VARCHAR(30) PRIMARY KEY,\n" + " clase VARCHAR(20) NOT NULL,\n" + " vida INT,\n" + " defensa INT,\n" + " ataque INT,\n" + " velocidad INT\n" + ");";
 
             statement.execute(createTableSQL);
 
 
-        } catch (JdbcSQLIntegrityConstraintViolationException e) {
-            System.err.println("El pokémon ya existe");
         } catch (SQLException ex) {
             throw new RuntimeException(ex);
         }
@@ -103,7 +104,17 @@ public class PokemonDAOImp implements PokemonDAO {
 
     @Override
     public void crear(Pokemon pokemon) throws PokemonRepeatedException {
-        System.out.println("creando");
+        try {
+            String insert = "INSERT INTO pokeapi (nombre,clase,vida,defensa,ataque,velocidad) " +
+                    "VALUES ('%s','%s',%d,%d,%d,%d);";
+
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(String.format(insert, pokemon.getNombre(), pokemon.getClase(),
+                    pokemon.getVida(), pokemon.getDefensa(), pokemon.getAtaque(), pokemon.getVelocidad()));
+        } catch (SQLException e) {
+            System.err.println("El pokémon ya existe");
+        }
+
     }
 
 
