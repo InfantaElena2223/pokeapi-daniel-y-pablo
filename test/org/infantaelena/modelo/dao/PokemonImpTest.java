b3d6787bd719e1087ea2509f8aa03a1c4ec6ebfd
daniotimon28 <daniotimon@gmail.com>
2023-05-19 13:13:19 +0200

    "19-5"

--- test/org/infantaelena/modelo/dao/PokemonImpTest.java
+++ test/org/infantaelena/modelo/dao/PokemonImpTest.java
@@ -1,13 +1,31 @@
 package org.infantaelena.modelo.dao;
 
-import org.infantaelena.controlador.Controlador;
-import org.infantaelena.vista.Vista;
+import org.infantaelena.excepciones.PokemonNotFoundException;
+import org.infantaelena.excepciones.PokemonRepeatedException;
+import org.infantaelena.modelo.entidades.Pokemon;
+import org.opentest4j.AssertionFailedError;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class PokemonImpTest {
 
     @org.junit.jupiter.api.Test
     void crear() {
+        PokemonDAOImp modelo = new PokemonDAOImp();
+        try {
+            modelo.crear(new Pokemon("PIKACHU", Pokemon.Clases.AIRE, 1, 2, 3, 4));
+            try {
+                Pokemon poke = modelo.leerPorNombre("peter");
+                assertEquals("a", poke.getNombre());
+            } catch (PokemonNotFoundException e) {
+                throw new AssertionFailedError();
+            }
+        } catch (AssertionFailedError error) {
+            System.err.println("No se ha ejecutado bien la operación");
+            System.out.println("Prueba a borrar la base de datos");
+        } catch (PokemonRepeatedException e) {
 
+        }
     }
 
     @org.junit.jupiter.api.Test
